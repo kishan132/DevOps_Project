07c638f797f3e36c87f6256805700842f47aa15f
kishan kumar <56019933+kishan132@users.noreply.github.com>
2021-05-30 22:05:46 +0530

    username updates

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-const MONGODB_URL = "mongodb://localhost:27017/EmployeeDB";
+const MONGODB_URL = "mongodb+srv://kishan:kishan123@cluster0.kiolc.mongodb.net/EmployeeDB?retryWrites=true&w=majority";
 const PORT = 5000;
 
 module.exports = { MONGODB_URL, PORT };
