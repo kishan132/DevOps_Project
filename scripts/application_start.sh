454c2bad851697a9ef5fb0ed00334817ddd67445
kishan kumar <56019933+kishan132@users.noreply.github.com>
2021-05-31 21:54:19 +0530

    syntax fix

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,7 +12,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
-exho "Installing dependencies..."
+echo "Installing dependencies..."
 npm install
 npm install nodemon -g
 
